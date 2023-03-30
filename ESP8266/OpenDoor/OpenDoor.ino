b0b89d5c5c4b94553491751091e201b86a657f81
Jobin Augustine <jobinau@gmail.com>
2023-03-30 16:20:22 +0530

    Web API for opening the door

--- ESP8266/OpenDoor/OpenDoor.ino
+++ ESP8266/OpenDoor/OpenDoor.ino
@@ -1,10 +1,11 @@
 /*  Open the Door after late night. Jobin Augustine */
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>  //UDP communication with NTP servers
+#include <ESP8266WebServer.h>  //Start a webserver
 
-/* D1 mini clone has LED connected to +ve, define according to behaviour */
-#define LEDON 0x0
+#define LEDON 0x0 // D1 mini clone has LED connected to +ve, define according to behaviour
 #define LEDOFF 0x1
+#define LOCKPIN 5 //GPIO number to which Solinoid lock is connected
 
 /*WiFi credentials*/
 #define ssid "ocean"
@@ -19,12 +20,16 @@ byte packetBuffer[NTP_PACKET_SIZE];  // buffer to hold incoming and outgoing pac
 WiFiUDP udp; // A UDP instance to let us send and receive packets over UDP
 unsigned long secsSince1900;  //Response from NTP server
 unsigned long epoch, startepoch ;  //Converted UNIX epoch
+unsigned long millisec; //Milliseconds since the startup
+bool doorClosed = true;  //When the program starts, door must be closed.
+
+ESP8266WebServer server(80);   //Webserver
 
 void setup() {
   Serial.begin(9600);     //Baud rate for Serial monitor
   pinMode(LED_BUILTIN, OUTPUT);  //LED as Output pin which is GPIO2 (D4)
-  pinMode(5, OUTPUT);  //D1	GPIO5 as output for door lock solinoid
-  digitalWrite(5, LOW);  //Make sure that solinoid is not active
+  pinMode(LOCKPIN, OUTPUT);  //GPIO output for door lock
+  digitalWrite(LOCKPIN, LOW);  //Make sure that solinoid is not active from the begining
 
   Serial.println();
   Serial.println();
@@ -44,6 +49,12 @@ void setup() {
   Serial.println("WiFi connected. IP address:");
   Serial.println(WiFi.localIP());
   blink();  
+  
+  server.on("/", handleRoot);               // Call the 'handleRoot' function when a client requests URI "/"
+  server.on("/noblink", noblink);
+  server.onNotFound(handleNotFound);        // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
+  server.begin();                           // Actually start the server
+  Serial.println("HTTP server started");
 
   /*Initialize the UPD */
   udp.begin(localPort);
@@ -73,17 +84,33 @@ void setup() {
 }
 
 void loop() {
-  unsigned long timeout = millis();
-  //Serial.printf("Epoch :%lu\n",epoch);
-  epoch = startepoch + (timeout/1000);
-  Serial.printf("The UTC time is %lu:%lu:%lu\n", (epoch % 86400L) / 3600, (epoch % 3600) / 60, epoch % 60);
+  unsigned long millisec = millis();
+  server.handleClient();                    // Listen for HTTP requests from clients  
+  epoch = startepoch + (millisec/1000);
+  Serial.printf("%lu ",millisec);
   digitalWrite(LED_BUILTIN, LEDON);
-  if (timeout > 14400000UL || ((epoch % 86400L) > 20 && (epoch % 3600) > 30))   //Trigger the Door unlock if conditions met
-    digitalWrite(5, HIGH);
-  delay(1000); 
+  if ( doorClosed &&  ( millisec > 14400000UL || ( (epoch % 86400L) / 3600 > 20 && (epoch % 3600) / 60 > 30)))   //Trigger the Door unlock if conditions met 7200000UL=2hrs  10800000UL=3hrs 14400000UL=4hrs
+    {
+      releaseDoor();
+    }
+  if (doorClosed)
+    delay(1000); 
+  else
+    blink();      //notification of dooropening attempt
   digitalWrite(LED_BUILTIN, LEDOFF);
-  digitalWrite(5, LOW);
-  delay(1000) ;
+  digitalWrite(LOCKPIN, LOW); //Make sure that lock is not active
+  delay(2000) ;
+}
+
+void releaseDoor(){
+  Serial.println("\n Releasing the Door at:");
+  Serial.printf("Time out %lu \n",millisec);
+  Serial.printf("Hour %lu \n",(epoch % 86400L) / 3600);
+  Serial.printf("Minute %lu \n",(epoch % 3600) / 60);
+  digitalWrite(LOCKPIN, HIGH);  //Trigger the solinoid
+  doorClosed = false;   //Status of the door is open
+  delay(1000);  //Hold it for a second
+  digitalWrite(LOCKPIN, LOW); //Explicitly release it back  
 }
 
 /* blink LED 3 TIMES */
@@ -120,4 +147,17 @@ void sendNTPpacket(IPAddress& address) {
   udp.beginPacket(address, 123);  // NTP requests are to port 123
   udp.write(packetBuffer, NTP_PACKET_SIZE); // all NTP fields have been given values, now we can send a packet requesting a timestamp:
   udp.endPacket();
+}
+
+void handleRoot() {
+  server.send(200, "text/plain", "Open Sesame!");   // Send HTTP status 200 (Ok) and send some text to the browser/client
+  releaseDoor();
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
+
+void noblink(){  //Stop the notification of door opening by setting it as doorClosed
+  doorClosed = true;
 }
