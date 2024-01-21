1770be651bb3c2ccba2d99f1f47549babd1ac88e
Jobin Augustine <jobinau@gmail.com>
2024-01-21 18:55:45 +0530

    Wificlient and https client are now globale objects

--- ESP8266/InverterController/InverterController/InverterController.ino
+++ ESP8266/InverterController/InverterController/InverterController.ino
@@ -10,7 +10,9 @@
 #define password "20010604"
 
 int BattVolt = 0;  // Battery voltage, Variable to store Output of ADC 
-int MainsState = 0; // Mains supply status, By deafult, Mains is there 
+int MainsState = 0; // Mains supply status, By deafult, Mains is there
+BearSSL::WiFiClientSecure client;
+HTTPClient https;
 
 /* Generic blink function. Blink LED cnt times with delay of dly */
 void blink(int cnt = 3,int dly = 100) {
@@ -38,6 +40,8 @@ void setup()
   Serial.println("");
   Serial.println("WiFi connected. IP address:");
   Serial.println(WiFi.localIP());
+  client.setInsecure();
+  https.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
   blink();
 }
 
@@ -45,14 +49,10 @@ void loop()
 {
   BattVolt = analogRead(analogPin); /* Read the Analog Input value */
   MainsState = digitalRead(MAINSPIN);
-  std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
-  client->setInsecure();
-  HTTPClient https;
-  https.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
-  if (https.begin(*client,"script.google.com",443,"/macros/s/AKfycbzr53VKDgBmnl4TvKvOjJX961NDY5esN6DKedwpObrTXCIjWpaZKxha0Q5CmKn37yUNpg/exec?item=10&action=R",true)){
+  if (https.begin(client,"script.google.com",443,"/macros/s/AKfycbzr53VKDgBmnl4TvKvOjJX961NDY5esN6DKedwpObrTXCIjWpaZKxha0Q5CmKn37yUNpg/exec?item=10&action=R",true)){
       Serial.print("[HTTPS] GET...\n");
       int httpCode = https.GET(); // start connection and send HTTP header
-      if (httpCode > 0) { // httpCode will be negative on error
+      if (httpCode > 0) {         // httpCode will be negative on error
         Serial.printf("[HTTPS] GET... code: %d\n", httpCode); // HTTP header has been send and Server response header has been handled
         if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {  // file found at server
           String payload = https.getString();
